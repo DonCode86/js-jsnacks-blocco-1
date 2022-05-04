913930ffd2b052485309ef851a9d776e63d39ed1
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-04 12:41:59 +0200

    snack 5

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -1,12 +1,19 @@
-const firstNumber = Number(prompt("inserisci il primo numero"));
-const secondNumber = Number(prompt("inserisci il secondo numero"));
-const thirdNumber = Number(prompt("inserisci il terzo numero"));
-const fourthNumber = Number(prompt("inserisci il quarto numero"));
-const fifthNumber = Number(prompt("inserisci il quinto numero"));
-const sixthNumber = Number(prompt("inserisci il sesto numero"));
-const seventhNumber = Number(prompt("inserisci il settimo numero"));
-const eighthNumber = Number(prompt("inserisci l'ottavo numero"));
-const ninthNumber = Number(prompt("inserisci il nono numero"));
-const tenthNumber = Number(prompt("inserisci il decimo numero"));
+// const firstNumber = Number(prompt("inserisci il primo numero"));
+// const secondNumber = Number(prompt("inserisci il secondo numero"));
+// const thirdNumber = Number(prompt("inserisci il terzo numero"));
+// const fourthNumber = Number(prompt("inserisci il quarto numero"));
+// const fifthNumber = Number(prompt("inserisci il quinto numero"));
+// const sixthNumber = Number(prompt("inserisci il sesto numero"));
+// const seventhNumber = Number(prompt("inserisci il settimo numero"));
+// const eighthNumber = Number(prompt("inserisci l'ottavo numero"));
+// const ninthNumber = Number(prompt("inserisci il nono numero"));
+// const tenthNumber = Number(prompt("inserisci il decimo numero"));
 
-console.log(firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber + sixthNumber + seventhNumber + eighthNumber + ninthNumber + tenthNumber);
+let total = 0;
+
+for (let i = 0; i < 10; i++) {
+    const userNumber = Number(prompt("inserisci un numero"));
+    total += userNumber;
+}
+
+console.log(total);
