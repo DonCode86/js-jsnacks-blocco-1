7846a88a0df56103ed24e34692c42108fdf9cd45
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-04 12:42:30 +0200

    snack 5 completato

---
+++ snack-5/js/main.js
@@ -0,0 +1,9 @@
+const numbersBox = []
+let userNumber;
+for (let i = 0; i < 6; i++) {
+    userNumber = Number(prompt("inserisci un numero"));
+    if (userNumber % 2 === 1) {
+        numbersBox.push(userNumber);
+        console.log(numbersBox);
+    }
+}
