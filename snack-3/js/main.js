51d2744266237b184bc0fabd4f60f38cc00d952d
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-04 12:05:53 +0200

    snack 3

---
+++ snack-3/js/main.js
@@ -0,0 +1,12 @@
+const firstNumber = Number(prompt("inserisci il primo numero"));
+const secondNumber = Number(prompt("inserisci il secondo numero"));
+const thirdNumber = Number(prompt("inserisci il terzo numero"));
+const fourthNumber = Number(prompt("inserisci il quarto numero"));
+const fifthNumber = Number(prompt("inserisci il quinto numero"));
+const sixthNumber = Number(prompt("inserisci il sesto numero"));
+const seventhNumber = Number(prompt("inserisci il settimo numero"));
+const eighthNumber = Number(prompt("inserisci l'ottavo numero"));
+const ninthNumber = Number(prompt("inserisci il nono numero"));
+const tenthNumber = Number(prompt("inserisci il decimo numero"));
+
+console.log(firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber + sixthNumber + seventhNumber + eighthNumber + ninthNumber + tenthNumber);
