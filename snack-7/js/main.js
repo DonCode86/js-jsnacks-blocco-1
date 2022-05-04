3cc1dcfa68287166edec018efdf0911fbae33c0d
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-04 17:02:09 +0200

    snack 7

--- snack-7/js/main.js
+++ snack-7/js/main.js
@@ -2,8 +2,9 @@ const firstName = ["Daniele", "Marco", "Giuseppe", "Mirko"];
 const secondName = ["Ciccarelli", "Rossi", "Verdi", "Esposito"];
 
 let list = [];
-
-const item = firstName[Math.floor(Math.random() * firstName.length)];
-const secondItem = secondName[Math.floor(Math.random() * firstName.length)];
-list.push(item + " " + secondItem);
+for (let i = 0; i < 3; i++) {
+    const item = firstName[Math.floor(Math.random() * firstName.length)];
+    const secondItem = secondName[Math.floor(Math.random() * firstName.length)];
+    list.push(item + " " + secondItem);
+}
 console.log(list);
